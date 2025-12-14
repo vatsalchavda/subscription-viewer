e43f7f463e634f7a00d1be86860cdf012e3c6542
Vatsal Chavda <vatsalchavda2@gmail.com>
2025-12-13 20:40:29 -0500

    fix: trigger deployment

--- amplify/functions/get-subscription/resource.ts
+++ amplify/functions/get-subscription/resource.ts
@@ -6,7 +6,6 @@ export const getSubscription = defineFunction({
   environment: {
     STRIPE_SECRET_KEY: secret('STRIPE_SECRET_KEY'),
     // In a real app, this would be a DB lookup. For now, we mock it via Env or Hardcoding.
-    DEFAULT_STRIPE_CUSTOMER_ID: secret('DEFAULT_STRIPE_CUSTOMER_ID'), 
-    
+    DEFAULT_STRIPE_CUSTOMER_ID: secret('DEFAULT_STRIPE_CUSTOMER_ID'),
   }
 });
